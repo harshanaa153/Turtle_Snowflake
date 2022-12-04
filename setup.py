1627ef6139e9a2f222aad3eb08977939c1f1c23a
harshanaa153 <harshanaa153@gmail.com>
2022-12-04 22:43:55 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+	name="snowflake",
+	version="0.1",
+	author="Harsha"
+	author_email="harshanaa153@gmail.com"
+	packages=find_packages(),
+	install_requires=["numpy","turtles","random"]
+)
