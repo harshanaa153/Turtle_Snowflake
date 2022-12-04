17b80af305c3aab0875a2ba235704bcec3f0a05a
harshanaa153 <harshanaa153@gmail.com>
2022-12-04 22:50:40 +0100

    essential details

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages
 setup(
 	name="snowflake",
 	version="0.1",
-	author="harshanaa153"
-	author_email="harshanaa153@gmail.com"
+	author="harshanaa153",
+	author_email="harshanaa153@gmail.com",
 	packages=find_packages(),
 	install_requires=["numpy","turtles","random"]
 )
